958bf60536233c94aff2d26ec56325fa0aa82c05
Blxucreep <LoevanLqc@outlook.com>
2024-01-30 08:45:25 +0100

    setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='adventure',
+    version='1.0',
+    author='Blxucreep',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
